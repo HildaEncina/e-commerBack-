0199f6141417f36e5b58221ff79349abf567f6e7
HildaEncina <encina.hilda@gmail.com>
2024-11-06 19:59:18 -0300

    feat(B01) Creacion del modelo de base de datos para usuarios

--- src/config/baseDatos.js
+++ src/config/baseDatos.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/e-commerce')
+mongoose.connect('mongodb://127.0.0.1:27017/vinoteca')
     .then(() => {
-        console.log("Conexión exitosa a la base de datos 'e-commerce'");
+        console.log("Conexión exitosa a la base de datos 'vinoteca'");
     })
     .catch((error) => {
         console.log(`Hubo un error al conectar con la base de datos: ${error}`);
---
+++ src/models/usuario.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+
+
+const usuarioShema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: [true, 'Por favor proporciona el nombre.'],
+        minLength: [3, 'Por favor proporciona un nombre real.']
+    },
+    apellido:{
+        type: String,
+        required: [true, 'Por favor proporcione su apellido'],
+        minLength: [3, 'Por favor proporciona un apellido mas largo']
+    },
+    edad:{
+        type: Number,
+        required: [true, 'Por favor proporciona la edad.'],
+    },
+    email:{
+        type: String,
+        required: [true, 'Por favor proporcione su correo.'],
+        minLength: [4, 'Por favor proporciona un correo real']
+    },
+    clave:{
+        type: String,
+        required: [true, 'Por favor proporcione su contrasena'],
+        minLength: [3, 'Por favor proporciona una contraseña mas segura']
+    },
+    telefono:{
+        type: Number,
+        minLength: [9, 'Por favor proporciona un numero de telefono real']
+    },
+    domicilio:{
+        type: String,
+        required: [true, 'Por favor proporciona el nombre.'],
+        minLength: [5, 'Por favor proporciona una ubicación real.']
+    }, 
+    rol:{
+        type: String,
+        required: [true],
+
+    }, 
+    avatar:{
+        type: String
+    },
+
+    carrito: [CarritoSchema]
+},
+{timestamps: true});
+
+const Usuario = mongoose.model('usuario', usuarioShema); 
+
+module.exports = Usuario; 
